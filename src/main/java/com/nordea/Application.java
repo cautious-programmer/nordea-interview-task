bef89d1c79aecf4531f4fce6d24b5e7697f47178
mrozak <michal.rozak@gmail.com>
2020-09-28 09:09:44 +0200

    add more logging

--- src/main/java/com/nordea/Application.java
+++ src/main/java/com/nordea/Application.java
@@ -1,13 +1,20 @@
 package com.nordea;
 
 import com.nordea.parser.FileParser;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
 
 import static com.nordea.validator.ArgumentValidator.validate;
 
+@Slf4j
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        log.info("application has started");
         validate(args);
-        new FileParser(args[0], args[1]);
+        log.info("passed application parameters are : [{}]", Arrays.toString(args));
+        new FileParser(args[0], args[1]).parse();
+        log.info("application has finished");
     }
 }
